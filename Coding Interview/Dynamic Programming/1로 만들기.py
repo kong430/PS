c527ace9870a7a8f2f178c13392a943b6d6eeb06
MinGyeong Park <37508296+kong430@users.noreply.github.com>
2022-06-24 10:45:48 +0900

    Update 1로 만들기.py

--- Coding Interview/Dynamic Programming/1로 만들기.py
+++ Coding Interview/Dynamic Programming/1로 만들기.py
@@ -6,10 +6,13 @@ for i in range(2, x+1):
     # 1을 뺴는 경우
     dp[i] = dp[i-1]+1
     # 2로 나누어 떨어질 경우
-    dp[i] = min(dp[i], dp[i//2]+1)
+    if i%2==0:
+        dp[i] = min(dp[i], dp[i//2]+1)
     # 3으로
-    dp[i] = min(dp[i], dp[i//3]+1)
+    if i%3==0:
+        dp[i] = min(dp[i], dp[i//3]+1)
     # 5로
-    dp[i] = min(dp[i], dp[i//5]+1)
+    if i%5==0:
+        dp[i] = min(dp[i], dp[i//5]+1)
 
 print(dp[x])
